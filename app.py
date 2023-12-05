ad534ad4cf14e376a0f75d473d385ae34fc14ac5
gauravbogar <62739430+gauravbogar@users.noreply.github.com>
2023-12-05 18:31:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
@@ -11,21 +10,17 @@ def index():
    print('Request for index page received')
    return render_template('index.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/hello', methods=['POST'])
 def hello():
    name = request.form.get('name')
 
    if name:
-       print('Request for hello page received with name=%s' % name)
+       print(f'Request for hello page received with name={name}')
        return render_template('hello.html', name = name)
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+       return '<p>No Name Found</p>'
 
 
 if __name__ == '__main__':
